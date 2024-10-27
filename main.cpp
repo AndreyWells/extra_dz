7814c72bde1ccee305b6f6a363c6b55ae83bccf2
Andrey <vikakaef@inbox.ru>
2024-10-27 18:47:07 +0500

    Created the structure for the BMP header

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include <fstream>
+#include <cstdint>
 using namespace std;
-
+struct BMPHeader {
+  uint16_t fileType;
+  uint32_t fileSize;
+  uint16_t reserved1;
+  uint16_t reserved2;
+  uint32_t offsetData;
+};
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
 }
